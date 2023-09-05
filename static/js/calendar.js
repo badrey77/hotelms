4f36c14803f8848ef96f70547f5556ec7db256a0
badrey77 <badrey77@gmail.com>
2023-09-05 23:10:04 +0100

    xyz

--- static/js/calendar.js
+++ static/js/calendar.js
@@ -9,15 +9,57 @@ $(document).ready(function() {
     });
     $.each($("rect[id^='rect']"), function () {
         $(this).mousedown(function (e) {
-            $(this).toggleClass('rect_B');
+                var selectedOption = $("rect.selected").first()[0];
+            console.log(selectedOption)
+                switch (selectedOption.id) {
+                    case "legende_L":
+                        $(this).toggleClass('rect_L');
+                        break;
+                    case "legende_B":
+                        $(this).toggleClass('rect_B');
+                        break;
+                    case "legende_C":
+                        $(this).toggleClass('rect_C');
+                        break;
+                    case "legende_T":
+                        $(this).toggleClass('rect_T');
+                        break;
+                    case "legende_O":
+                        $(this).toggleClass('rect_O');
+                        break;
+                    case "legende_N":
+                        $(this).toggleClass('rect_N');
+                        break;
+                    case "legende_M":
+                        $(this).toggleClass('rect_M');
+                        break;
+                }
         });
         $(this).hover(function (e) {
             if(e.buttons == 1){
                 var selectedOption = $("rect.selected").first()[0];
-                console.log(selectedOption.id)
-                switch (selectedOption) {
-                    case "rect_B":
-                        $(this).addClass('rect_B');
+                switch (selectedOption.id) {
+                    case "legende_L":
+                        $(this).toggleClass('rect_L');
+                        break;
+                    case "legende_B":
+                        $(this).toggleClass('rect_B');
+                        break;
+                    case "legende_C":
+                        $(this).toggleClass('rect_C');
+                        break;
+                    case "legende_T":
+                        $(this).toggleClass('rect_T');
+                        break;
+                    case "legende_O":
+                        $(this).toggleClass('rect_O');
+                        break;
+                    case "legende_N":
+                        $(this).toggleClass('rect_N');
+                        break;
+                    case "legende_M":
+                        $(this).toggleClass('rect_M');
+                        break;
                 }
             }
         }, function (e) {
